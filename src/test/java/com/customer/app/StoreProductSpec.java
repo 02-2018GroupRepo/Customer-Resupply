8fe4cca33e94fe7c692beeb6ca15fef54153a690
Tyler Campbell <tcampb30@gmail.com>
2018-04-10 13:27:48 -0400

    Fixed issue where productService randomId returned duplicate values

--- src/test/java/com/customer/app/StoreProductSpec.java
+++ src/test/java/com/customer/app/StoreProductSpec.java
@@ -1,7 +1,5 @@
 package com.customer.app;
 
-
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -16,58 +14,58 @@ import com.customer.app.model.product.Product;
 import com.customer.app.service.ProductService;
 
 public class StoreProductSpec {
-	private HashMap<Integer, Integer> testStoreList;
-	@Before
-	public void setup() {
-//		Inventory in Store
-		testStoreList = new HashMap<>();
-
-		testStoreList.put(1, 15);
-		testStoreList.put(3, 30);
-		testStoreList.put(56, 0);
-		testStoreList.put(45, 10);
-		testStoreList.put(25, 0);
-		testStoreList.put(9, 15);
-		testStoreList.put(22, 70);
-		testStoreList.put(54, 20);
-		testStoreList.put(58, 3);
-		testStoreList.put(21, 8);
-
-	}
+	
+//	HashMap<Integer, Integer> testStoreList;
+//
+//	@Before
+//	public void setup() {
+//		// Inventory in Store
+//		testStoreList = new HashMap<>();
+//
+//		testStoreList.put(1, 15);
+//		testStoreList.put(3, 30);
+//		testStoreList.put(56, 0);
+//		testStoreList.put(45, 10);
+//		testStoreList.put(25, 0);
+//		testStoreList.put(9, 15);
+//		testStoreList.put(22, 70);
+//		testStoreList.put(54, 20);
+//		testStoreList.put(58, 3);
+//		testStoreList.put(21, 8);
+//
+//	}
 
 	@Test
 	public void storeTest() {
+		 HashMap<Integer, Integer> testStoreList = new HashMap<>();
 		
-		testStoreList = new HashMap<>();
+		 testStoreList.put(1, 15);
+		 testStoreList.put(2, 30);
+		 testStoreList.put(3, 0);
+		 testStoreList.put(4, 10);
+		 testStoreList.put(5, 0);
+		 testStoreList.put(6, 15);
+		 testStoreList.put(7, 70);
+		 testStoreList.put(8, 20);
+		 testStoreList.put(9, 3);
+		 testStoreList.put(10, 8);
 
-		testStoreList.put(1, 15);
-		testStoreList.put(3, 30);
-		testStoreList.put(56, 0);
-		testStoreList.put(45, 10);
-		testStoreList.put(25, 0);
-		testStoreList.put(9, 15);
-		testStoreList.put(22, 70);
-		testStoreList.put(54, 20);
-		testStoreList.put(58, 3);
-		testStoreList.put(21, 8);
-		
-		
-		
-//		create post order object
-		HashMap<Integer , Integer> orderMap = new HashMap<>();
-		ArrayList<Integer> randomProductIDs = (ArrayList<Integer>) ProductService.randomProductIds(60);
-//		set 20 random item id and quantity where the store quantity is not 0
+		// create post order object
+		System.out.println();
+		HashMap<Integer, Integer> orderMap = new HashMap<>();
+		ArrayList<Integer> randomProductIDs = (ArrayList<Integer>) ProductService.randomProductIds(10);
+		// set 20 random item id and quantity where the store quantity is not 0
 		int i = 0;
-		while(orderMap.size() < 8) {
+		while (orderMap.size() < 8) {
 			int productQuantity = testStoreList.get(randomProductIDs.get(i));
 			if (productQuantity > 0) {
-				orderMap.put(i, (int) (Math.floor(Math.random()* productQuantity) + 1));
+				orderMap.put(i, (int) (Math.floor(Math.random() * productQuantity) + 1));
 			}
 			i++;
 		}
-		assertTrue(orderMap.size() == 7);
-		for (int j = 0; j < orderMap.size(); j++) {
-			assertTrue(orderMap.get(i) > 0);
-		}
+		assertTrue(orderMap.size() == 8);
+		assertTrue(orderMap.get(1) > 0);
+		assertTrue(orderMap.get(7) > 0);
+		assertTrue(orderMap.get(8) > 0);
 	}
 }
